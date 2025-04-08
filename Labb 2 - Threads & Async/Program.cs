a6372240cb662f691a3a47d55a981cce7f57382a
Conixen <leon.json57@gmail.com>
2025-04-08 10:30:24 +0200

    Add project files.

---
+++ Labb 2 - Threads & Async/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb_2___Threads___Async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
